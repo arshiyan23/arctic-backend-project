5784bfd80b17e0fc279f8c0d9288fb530ecbe443
arshiyan23 <stardash97@gmail.com>
2026-05-22 15:02:20 +0400

    Fix startup.sh with all path replacements

--- startup.sh
+++ startup.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 sed -i 's|root /home/site/wwwroot;|root /home/site/wwwroot/web;|' /etc/nginx/sites-enabled/default
+sed -i 's|/home/site/wwwroot/index|/home/site/wwwroot/web/index|' /etc/nginx/sites-enabled/default
+sed -i 's|/home/site/wwwroot\$fastcgi_script_name|/home/site/wwwroot/web\$fastcgi_script_name|' /etc/nginx/sites-enabled/default
+exit 0
