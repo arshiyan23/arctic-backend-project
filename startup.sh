0832a1a230b4fc2e9f0f8ef4dc595ca91fcaa5bb
arshiyan23 <stardash97@gmail.com>
2026-06-04 21:31:49 +0400

    fix: revert startup.sh - remove python3 nginx corruption, add debug dump

--- startup.sh
+++ startup.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 CFG=/etc/nginx/sites-enabled/default
 
-# Update doc root and script paths for web/ subdir
+# Backup
+cp $CFG /etc/nginx/sites-enabled/default.bak
+
+# Change doc root to web/
 sed -i 's|root /home/site/wwwroot;|root /home/site/wwwroot/web;|' $CFG
-sed -i 's|/home/site/wwwroot/index|/home/site/wwwroot/web/index|' $CFG
-sed -i 's|/home/site/wwwroot\$fastcgi_script_name|/home/site/wwwroot/web\$fastcgi_script_name|' $CFG
+sed -i 's|/home/site/wwwroot/index |/home/site/wwwroot/web/index |' $CFG
 
-# Remove all if-block security rules that block brackets in query strings
-python3 << 'PYEOF'
-import re
-with open('/etc/nginx/sites-enabled/default') as f:
-    c = f.read()
-
-# Remove if ($args ~ ... ) { ... } blocks (including multiline)
-c = re.sub(r'\s*if\s*\(\s*\$args\s*~.*?\{[^}]*\}', '', c, flags=re.DOTALL)
-# Remove if ($request_uri ~ ... ) { ... } blocks
-c = re.sub(r'\s*if\s*\(\s*\$request_uri\s*~.*?\{[^}]*\}', '', c, flags=re.DOTALL)
-
-with open('/etc/nginx/sites-enabled/default', 'w') as f:
-    f.write(c)
-PYEOF
+# Copy nginx configs to web root for debugging
+mkdir -p /home/site/wwwroot/web/nginx-debug
+cp $CFG /home/site/wwwroot/web/nginx-debug/
+find /etc/nginx -type f -name '*.conf' -exec cp {} /home/site/wwwroot/web/nginx-debug/ \;
+find /etc/nginx -type f -name 'default' -exec cp {} /home/site/wwwroot/web/nginx-debug/ \;
 
 exit 0
