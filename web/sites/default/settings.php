2ea07531398155554e131415d61e26e06b4d5b5e
arshiyan23 <stardash97@gmail.com>
2026-06-18 19:25:49 +0400

    Restore local DDEV database connection, remove Azure MySQL override

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -880,6 +880,7 @@ if (getenv('IS_DDEV_PROJECT') == 'true' && file_exists(__DIR__ . '/settings.ddev
   include __DIR__ . '/settings.ddev.php';
 }
 
+
 /**
  * Load local development override configuration, if available.
  *
