6f8c9d368de5c725a5d9136e19994973186517f0
arshiyan23 <stardash97@gmail.com>
2026-06-20 19:31:35 +0400

    Add explicit Azure rewrites for Drupal API paths

--- startup.sh
+++ startup.sh
@@ -6,4 +6,6 @@ sed -i 's|try_files \$uri \$uri/ =404;|try_files \$uri \$uri/ /index.php\$uri\$i
 sed -i 's|try_files \$uri /index.php?\$query_string;|try_files \$uri \$uri/ /index.php\$uri\$is_args\$args;|' /etc/nginx/sites-enabled/default
 grep -q 'try_files \$uri \$uri/ /index.php\$uri\$is_args\$args;' /etc/nginx/sites-enabled/default || \
   sed -i '/location \/ {/a\        try_files $uri $uri/ /index.php$uri$is_args$args;' /etc/nginx/sites-enabled/default
+grep -q 'location \^~ /jsonapi/' /etc/nginx/sites-enabled/default || \
+  sed -i '/location \/ {/i\    location = /jsonapi { rewrite ^ /index.php$request_uri last; }\n    location ^~ /jsonapi/ { rewrite ^ /index.php$request_uri last; }\n    location ^~ /subrequests { rewrite ^ /index.php$request_uri last; }\n    location ^~ /sites/default/files/styles/ { try_files $uri /index.php$uri$is_args$args; }\n' /etc/nginx/sites-enabled/default
 exit 0
