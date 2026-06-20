c0a33f7b6f2984ee9ae530f9326b75dde5be50d9
arshiyan23 <stardash97@gmail.com>
2026-06-20 19:41:31 +0400

    Revert "Route Azure clean URLs through Drupal front controller"
    This reverts commit d01306de6d927178584293325ff54d5faa8f11ce.

--- startup.sh
+++ startup.sh
@@ -2,8 +2,7 @@
 sed -i 's|root /home/site/wwwroot;|root /home/site/wwwroot/web;|' /etc/nginx/sites-enabled/default
 sed -i 's|/home/site/wwwroot/index|/home/site/wwwroot/web/index|' /etc/nginx/sites-enabled/default
 sed -i 's|/home/site/wwwroot\$fastcgi_script_name|/home/site/wwwroot/web\$fastcgi_script_name|' /etc/nginx/sites-enabled/default
-sed -i 's|try_files \$uri \$uri/ =404;|try_files \$uri \$uri/ /index.php\$uri\$is_args\$args;|' /etc/nginx/sites-enabled/default
-sed -i 's|try_files \$uri /index.php?\$query_string;|try_files \$uri \$uri/ /index.php\$uri\$is_args\$args;|' /etc/nginx/sites-enabled/default
-grep -q 'try_files \$uri \$uri/ /index.php\$uri\$is_args\$args;' /etc/nginx/sites-enabled/default || \
-  sed -i '/location \/ {/a\        try_files $uri $uri/ /index.php$uri$is_args$args;' /etc/nginx/sites-enabled/default
+sed -i 's|try_files \$uri \$uri/ =404;|try_files \$uri /index.php?\$query_string;|' /etc/nginx/sites-enabled/default
+grep -q 'try_files \$uri /index.php?\$query_string;' /etc/nginx/sites-enabled/default || \
+  sed -i '/location \/ {/a\        try_files $uri /index.php?$query_string;' /etc/nginx/sites-enabled/default
 exit 0
