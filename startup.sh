5daf6e1f4e55e245b936026dde7cf449750c9770
arshiyan23 <stardash97@gmail.com>
2026-06-20 19:41:31 +0400

    Revert "Enable clean URL routing on Azure"
    This reverts commit 5e8ff376f86c7aba5f33571408d0fc7077d00892.

--- startup.sh
+++ startup.sh
@@ -2,5 +2,4 @@
 sed -i 's|root /home/site/wwwroot;|root /home/site/wwwroot/web;|' /etc/nginx/sites-enabled/default
 sed -i 's|/home/site/wwwroot/index|/home/site/wwwroot/web/index|' /etc/nginx/sites-enabled/default
 sed -i 's|/home/site/wwwroot\$fastcgi_script_name|/home/site/wwwroot/web\$fastcgi_script_name|' /etc/nginx/sites-enabled/default
-sed -i 's|try_files \$uri \$uri/ =404;|try_files \$uri /index.php?\$query_string;|' /etc/nginx/sites-enabled/default
 exit 0
